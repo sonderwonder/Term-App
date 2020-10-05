0a7f94df0a6c153871b9499679ef96f522e652cd
Stuart Atholwood <stuartatholwood@192-168-1-149.tpgi.com.au>
2020-10-06 01:19:39 +1100

    Added Event class to event.rb and added launch_menu to menu.rb

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,1 +1,19 @@
-bleep
+class Menu
+
+    def initialize 
+
+    end 
+
+    def launch_menu 
+        puts "\n"
+        puts "Welcome to the Organisi:"
+        puts "\n"
+        puts "would you like to:"
+        puts "1. View your Calander"
+        puts "2. Schedule a new event"
+    end
+end
+
+session = Menu.new
+
+session.launch_menu
